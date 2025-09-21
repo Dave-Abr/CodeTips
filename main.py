da40b7172ba148a5b5e10e07ea3df6b3d38811a9
David Git <davidfelipeabril@gmail.com>
2025-09-21 13:49:38 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import module1
+
+# var= module1.add(5,6)
+# print(f'{var = }')
+
+parameter = 3
+result = lambda parameter: parameter**2
+
+print(f'{result =}')
+
+
---
+++ module1.py
@@ -0,0 +1,7 @@
+if __name__=="__main__":
+    print(__name__)
+else:
+    print('Module Imported')
+
+def add(x,y):
+    return x+y
